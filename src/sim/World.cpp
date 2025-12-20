a614a723db3b5a5ed241a5cdd61dd0d52d706054
Jad Chahin <98987031+jad-chahin@users.noreply.github.com>
2025-12-19 21:24:03 -0500

    Made world class and commented plan

---
+++ src/sim/World.cpp
@@ -0,0 +1,8 @@
+//
+// Created by jchah on 2025-12-19.
+//
+
+#include "World.h"
+
+namespace sim {
+} // sim
---
+++ src/sim/World.h
@@ -0,0 +1,13 @@
+//
+// Created by jchah on 2025-12-19.
+//
+
+#ifndef PHYSICS3D_WORLD_H
+#define PHYSICS3D_WORLD_H
+
+namespace sim {
+    class World {
+    };
+} // sim
+
+#endif //PHYSICS3D_WORLD_H
